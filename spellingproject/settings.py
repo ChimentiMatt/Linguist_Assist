c2e1a62f3f2ef5777fca178964395a6cad58dad6
matthew chimenti <matthewosu@gmail.com>
2021-11-11 18:03:53 -0800

    made members and a registeration page that is rendering without content

--- spellingproject/settings.py
+++ spellingproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sass_processor',
     'spellingapp',
+    'members',
 ]
 
 STATICFILES_FINDERS = [
--- spellingproject/urls.py
+++ spellingproject/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('', include('spellingapp.urls')),
     path('admin/', admin.site.urls),
-
+    path('', include('spellingapp.urls')),
+    
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
+
