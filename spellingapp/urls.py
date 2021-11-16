2f4b2d751720a72ca6624329dbee86b493a6eb16
matthew chimenti <matthewosu@gmail.com>
2021-11-16 14:58:40 -0800

    learning page made. Now vue.js and building of game can be done

--- spellingapp/urls.py
+++ spellingapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', BaseView, name="home"),
     path('HomeView/', views.HomeView, name="HomeView" ),
     path('study/', views.study_view, name="study_view" ),
+    path('learning/', views.learning_view, name="learning_view"),
     path('remove_key_val/<int:pk>/', views.remove_key_val, name='remove_key_val'),
     path('add_key_val/', views.add_key_val, name='add_key_val')
 ]
--- spellingapp/views.py
+++ spellingapp/views.py
@@ -19,6 +19,9 @@ def HomeView(request):
     }
     return render(request, 'spellingapp/user_home.html', context)
 
+def learning_view(request):
+    return render(request, 'spellingapp/learning.html')
+
 
 def remove_key_val(request, pk):
     word_delete = get_object_or_404(KeyVal, pk=pk)
@@ -26,8 +29,12 @@ def remove_key_val(request, pk):
     return redirect('spellingapp:study_view')
 
 def add_key_val(request):
-    word = request.POST.get('word')
-    spelling_error = request.POST.get('spelling_error')
+    word = request.POST.get('word').title()
+    spelling_error = request.POST.get('spelling_error').title()
+
+    if word == '' or spelling_error == '':
+        return redirect('spellingapp:study_view')  
+        
     KeyVal.objects.create(
         word=word,
         spelling_error=spelling_error,
